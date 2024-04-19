1b953dc7607b46b6d37bf2ac9fed7b65cc6ae87d
alicechang29 <147457604+alicechang29@users.noreply.github.com>
2024-04-18 17:41:53 -0700

    updated docstrings

--- js/dom.js
+++ js/dom.js
@@ -9,9 +9,11 @@ export const $storiesLists = document.querySelector(".stories-list");
 export const $loginForm = document.querySelector("#LoginForm");
 export const $signupForm = document.querySelector("#SignupForm");
 
+// selectors for the form elements for "Add Story" submission
 export const $storyForm = document.querySelector("#submitStoryForm");
 export const $storyFormBtn = document.querySelector("#submitStoryBtn");
 
+// selectors for the nav bar
 export const $navLogin = document.querySelector("#Nav-login");
 export const $navUserProfile = document.querySelector("#Nav-userProfile");
 export const $navLogOut = document.querySelector("#Nav-logout");
